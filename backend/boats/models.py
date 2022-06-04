d79a72d4b704f036b058ac79549e798cc7384164
ARyback <adam.ryback@gmail.com>
2022-06-04 11:58:54 -0500

    Added Boat class and began filling fields. Needs work.

--- backend/boats/models.py
+++ backend/boats/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+class Boat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    boat_type = models.CharField(max_length=30)
+    capacity = models.IntegerField()
+    picture = models.IntegerField()
+    boat_rating = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    lake = models.CharField(max_length=100)
+    
