22701242cb2d1930fb3c5a5d7c8527fe62213764
ARyback <adam.ryback@gmail.com>
2022-06-04 18:28:59 -0500

    Made  Boat Serializer Class and File

---
+++ backend/boats/serializers.py
@@ -0,0 +1,8 @@
+import imp
+from rest_framework import serializers
+from .models import Boat
+
+class BoatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Boat
+        fields = ['id', 'capacity', 'description', 'location', 'lake', 'picture', 'boat_rating', 'boat_type', 'user_id']
