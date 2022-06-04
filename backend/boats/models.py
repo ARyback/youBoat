1cea39fb80ce4117e25fa04b3ed51e7ebc6f8ad1
ARyback <adam.ryback@gmail.com>
2022-06-04 18:09:59 -0500

    Added boats to settings.py installed_apps and migrate the boats app

--- backend/boats/models.py
+++ backend/boats/models.py
@@ -4,11 +4,42 @@ from authentication.models import User
 # Create your models here.
 class Boat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    boat_type = models.CharField(max_length=30)
     capacity = models.IntegerField()
-    picture = models.IntegerField()
-    boat_rating = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
     location = models.CharField(max_length=100)
     lake = models.CharField(max_length=100)
+    picture = models.ImageField()
+    LOW = 'LO'
+    MEDIUM = 'ME'
+    HIGH = 'HI'
+    BOAT_RATINGS = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    ]
+    boat_rating = models.CharField(
+        max_length=2,
+        choices=BOAT_RATINGS,
+        default=MEDIUM,
+        )
+    PONTOON = 'PO'
+    FISHING = 'FI'
+    CHARTERFISHING = 'CH'
+    JETSKI = 'JE'
+    SPEEDBOAT = 'SP'
+    BOAT_CHOICES = [
+        (PONTOON, 'Pontoon'),
+        (FISHING, 'Fishing'),
+        (CHARTERFISHING, 'Charterfishing'),
+        (JETSKI,'Jetski'),
+        (SPEEDBOAT, 'Speedboat'),
+    ]
+    boat_type = models.CharField(
+        max_length=2,
+        choices=BOAT_CHOICES,
+        default=PONTOON,
+        )
+
+
+    
     
